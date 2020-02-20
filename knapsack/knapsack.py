59db2c1af8f14e2fe4781c48c2b2e1377d2f54db
Aaron Clark <aaronpclark92@gmail.com>
2020-02-19 23:24:30 -0600

    completed stock_prices.py, accomplished some work on knapsack

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -3,24 +3,102 @@
 import sys
 from collections import namedtuple
 
+
+# class Item:
+#     def __init__(self, index, size, value):
+#         self.index = index
+#         self.size = size
+#         self.value = value
+
+#     def __str__(self):
+#         return f'{self.index}  {self.size}  {self.value}'
+
+
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+
+#  * Helper function to evaluate the value of an inventory
+def evaluate_value(inventory_1, inventory_2):
+    inventory_1_value = 0
+    inventory_2_value = 0
+    for i in range(0, len(inventory_1)-1):
+        inventory_1_value = + inventory_1[i].value
+    for j in range(0, len(inventory_2)-1):
+        inventory_2_value += inventory_2[j].value
+
+
+# * Start with capacity of 0, find items to add that meet capacity 0. Then, move on to capacity 1, find items that meet capacity 1
+
+
 def knapsack_solver(items, capacity):
-  pass
-  
+    print(items)
+    # considered_items = []
+    response = {
+        "value": 0,
+        "chosen": []
+    }
+
+    for i in range(0, len(items)-1, -1):
+        print(items[i])
+        index = items[i].index
+        size = items[i].size
+        value = items[i].value
+        if len(items) == 0 or capacity == 0:
+            return 0
+        elif size > capacity:
+          # * if the size of current item is greater than capacity, test out the item to the left
+            return knapsack_solver(items[:index-1], capacity)
+        else:
+            inventory_1 = knapsack_solver(items[:index-1], capacity)
+            response.value += value
+            response.chosen.append(items[i])
+            inventory_2 = knapsack_solver(items[:index-1], capacity-size)
+
+        return response
+
+        # result = evaluate_value(inventory_1, inventory_2)
+        # capacity -= items[i].size
+        # value += items[i].value
+
+        knapsack_solver(items[:i], capacity)
+
+# * Loop through an array of 0 - capacity
+    # for i in range(0, capacity):
+
+        # for j in range(0, items):
+        #   if items[j].value <= capacity and items[j].value >= 0:
+        #     inventory.append(items[j])
+
+    # for item in items:
+    #     # print(f'Currently looking at {item}')
+
+    #     if item.size > inventory_space:
+    #         pass
+
+    #     else:
+    #         inventory.append(item)
+    #         capacity -= item.size
+    #     knapsack_solver(items[item.index:], capacity, inventory)
+    #     # if items[index]:
+
+    # if evaluate_value(inventory) > evaluate_value(inventory_original):
+    #     return inventory
+    # else:
+    #     return inventory_original
+
 
 if __name__ == '__main__':
-  if len(sys.argv) > 1:
-    capacity = int(sys.argv[2])
-    file_location = sys.argv[1].strip()
-    file_contents = open(file_location, 'r')
-    items = []
+    if len(sys.argv) > 1:
+        capacity = int(sys.argv[2])
+        file_location = sys.argv[1].strip()
+        file_contents = open(file_location, 'r')
+        items = []
 
-    for line in file_contents.readlines():
-      data = line.rstrip().split()
-      items.append(Item(int(data[0]), int(data[1]), int(data[2])))
-    
-    file_contents.close()
-    print(knapsack_solver(items, capacity))
-  else:
-    print('Usage: knapsack.py [filename] [capacity]')
+        for line in file_contents.readlines():
+            data = line.rstrip().split()
+            items.append(Item(int(data[0]), int(data[1]), int(data[2])))
+
+        file_contents.close()
+        print(knapsack_solver(items, capacity))
+    else:
+        print('Usage: knapsack.py [filename] [capacity]')
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,68 @@
 
 import argparse
 
+
+def selection_sort(arr):
+
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[i] and arr[j] < arr[smallest_index]:
+                arr[cur_index], arr[j] = arr[j], arr[cur_index]
+            j += 1
+    return arr
+
+
+# * takes list of stock prices
+
 def find_max_profit(prices):
-  pass
+  # * loop through prices, find the lowest number that comes before the highest number
+    purchase_prices = prices[:-1]
+    sale_prices = prices[1:]
+    differences = []
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+# * iterate through sale prices in order to subtract each purchase price from a sale price, appending the result to differences
+    for i in range(0, len(purchase_prices)):
+      # * j has range starting at  i to preserve the purchase-then-sale order.
+        for j in range(i, len(sale_prices)):
+            difference = sale_prices[j]-purchase_prices[i]
+            differences.append(difference)
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    differences = selection_sort(differences)
+    return differences[len(differences)-1]
+    # print(differences)
+
+    # * iterate over zeroth through end-1 index of prices to fidn min vals
+
+
+# # * iterate over prices to find max
+    # for i in prices[1:]:
+    #     if i > highest_price:
+    #         highest_price = i
+    #         highest_price_idx = prices.index(i)
+# # * iterate over prices up to highest price, and create new array from highest_price minus current iteration value
+#     for j in prices[:highest_price_idx]:
+#         differences.append(highest_price-j)
+
+#     # * Iterate over differences array and find largest value (similar to finding max value above). Assign profit to that value
+#     for x in differences:
+#         if x > profit:
+#             profit = x
+
+    # return profit
+
+
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
+
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#     parser = argparse.ArgumentParser(
+#         description='Find max profit from prices.')
+#     parser.add_argument('integers', metavar='N', type=int,
+#                         nargs='+', help='an integer price')
+#     args = parser.parse_args()
+
+#     print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+#         profit=find_max_profit(args.integers), prices=args.integers))
